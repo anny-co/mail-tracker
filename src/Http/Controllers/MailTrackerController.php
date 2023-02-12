dbde055f982bedeae708e45895b8ad311f0a03bd
Oliver Wycisk <oliver.wycisk@gmail.com>
2023-02-12 17:13:54 +0100

    fix: use response helper to create response

--- src/Http/Controllers/MailTrackerController.php
+++ src/Http/Controllers/MailTrackerController.php
@@ -16,7 +16,7 @@ class MailTrackerController extends Controller
     {
         // Create a 1x1 transparent pixel and return it
         $pixel = sprintf('%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c', 71, 73, 70, 56, 57, 97, 1, 0, 1, 0, 128, 255, 0, 192, 192, 192, 0, 0, 0, 33, 249, 4, 1, 0, 0, 0, 0, 44, 0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 2, 68, 1, 0, 59);
-        $response = Response::make($pixel, 200);
+        $response = response($pixel, 200);
         $response->header('Content-type', 'image/gif');
         $response->header('Content-Length', 42);
         $response->header('Cache-Control', 'private, no-cache, no-cache=Set-Cookie, proxy-revalidate');
